6a01c1c342eb162d3e52d194b7465799c02a910c
Maxim Bazhenov <stadibenker@gmail.com>
2024-10-01 14:40:53 +0100

    added authentication

--- SecureApp/Pages/Account/Login.cshtml.cs
+++ SecureApp/Pages/Account/Login.cshtml.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SecureApp.Models.Account;
+using System.Security.Claims;
 
 namespace SecureApp.Pages.Account
 {
@@ -13,6 +15,28 @@ namespace SecureApp.Pages.Account
 		{
 		}
 
-		public void OnPost() { }
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+				return Page();
+
+			if (Credential.UserName == "admin" && Credential.Password == "password")
+			{
+				var claims = new List<Claim>
+				{
+					new Claim(ClaimTypes.Name, "admin"),
+					new Claim(ClaimTypes.Email, "t@g.c")
+				};
+
+				var identity = new ClaimsIdentity(claims, "CookieAuth");
+				ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+				await HttpContext.SignInAsync("CookieAuth", claimsPrincipal);
+
+				return RedirectToPage("/Index");
+			}
+
+			return Page();
+		}
 	}
 }
